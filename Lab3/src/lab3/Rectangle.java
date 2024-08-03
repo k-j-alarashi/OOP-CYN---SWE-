9c0de01d83269bc24a781dc7f446d80bb5ae9f1a
Khaled Jalal Al-arashi <77218749+k-j-alarashi@users.noreply.github.com>
2024-08-03 23:48:52 +0300

    Add files via upload

---
+++ Lab3/src/lab3/Rectangle.java
@@ -0,0 +1,48 @@
+package lab3;
+
+public class Rectangle {
+
+    int x;
+    int y;
+    double width;
+    double height;
+
+    public Rectangle(int x, int y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
