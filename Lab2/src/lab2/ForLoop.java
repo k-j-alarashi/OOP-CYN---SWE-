69f810399575ed21f282959d054f1ad758a044d6
Khaled Jalal Al-arashi <77218749+k-j-alarashi@users.noreply.github.com>
2024-08-16 18:34:40 +0300

    Delete Lab2 directory

--- Lab2/src/lab2/ForLoop.java
+++
@@ -1,9 +0,0 @@
-package lab2;
-
-public class ForLoop {
-    public static void main(String[] args) {
-        for (int i = 1; i <=12; i++) {
-            System.out.println(i+" x "+2+" = "+(i*2));   
-        }
-    }
-}
